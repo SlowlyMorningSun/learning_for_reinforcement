626d2bb5a76a8feed6fa29b16cc78fac78786941
Chenyang, Xu <41114394+SlowlyMorningSun@users.noreply.github.com>
2021-02-22 19:39:05 +0800

    Update value_function_approximate.py

--- value_function_approximate.py
+++ value_function_approximate.py
@@ -12,10 +12,12 @@ from sklearn.kernel_approximation import RBFSampler as RBF
 
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from Tools.scripts import lll
+import time
 
 # 设置显示方式
 # %matplotlib inline
-matplotlib.style.use('ggplot')
+# matplotlib.style.use('ggplot')
 
 env = gym.envs.make("MountainCar-v0")
 
@@ -24,11 +26,11 @@ class EStimator():
     def __init__(self):
         # 对环境进行采样，便于后续对于状态state抽取特征
         state_examples = np.array([env.observation_space.sample() for x in range(10000)])
-
+        # print(state_examples)
         # 特征处理1：归一化状态数据为0均值和单位方差
         self.scaler = Scaler()
         self.scaler.fit(state_examples)
-
+        
         # 特征处理2：状态state的特征抽取表示
         self.featurizer = FeatureUnion([("rbf1", RBF(gamma=5.0, n_components=100)),("rbf2",RBF(gamma=1.0, n_components=100)),])
         self.featurizer.fit(self.scaler.transform(state_examples))
@@ -133,7 +135,8 @@ class VF_QLearning():
             # 声明策略
             policy = self.epislon_greedy_policy(self.nA, policy_epislon)
             # 记录奖励
-            last_reward = self.record.episode_rewards(i_episode - 1)
+            last_reward = self.record.episode_rewards[i_episode - 1]
+            # print(last_reward)
             sys.stdout.flush()
 
             # 重置环境进行第一个动作
@@ -147,22 +150,27 @@ class VF_QLearning():
                 # 根据策略获得当前状态信号的动作值
                 action_probs = policy(state)
                 action = self.random_action(action_probs)
+                # print(action)
 
                 # 向前执行一步
                 next_state, reward, done, _ = env.step(action)
 
+                # print("reward",reward)
+
                 # 更新统计信息
                 # 更新信号奖励
+                self.record.episode_rewards[i_episode] += reward
+                # 更新迭代次数
                 self.record.episode_lengths[i_episode] = t
 
                 # 预测下一时间步的动作值
                 # 时间差分更新
-                q_values_next = EStimator.predict(next_state)
+                q_values_next = estimator.predict(next_state)
 
                 # 使用时间差分目标作为预测结果更新函数的近似器
                 td_target = reward + self.discount_factor * np.max(q_values_next)
                 # Q-Value 时间差分目标
-                EStimator.update(state, action, td_target)
+                estimator.update(state, action, td_target)
 
                 print("\rStep {} with reward ({})".format(t, last_reward),end="")
                 if done:break
@@ -172,3 +180,62 @@ class VF_QLearning():
         return self.record
 
 
+def plot_cost_to_go_mountain_car(env, estimator, niter, num_tiles = 20):
+    # 显示mountain Car 爬山车游戏的代价函数
+    # x 为 小车的位置信息
+    # y 为 小车的速度信息
+    # z 为 动作状态价值信息
+
+    x = np.linspace(env.observation_space.low[0], env.observation_space.high[0], num=num_tiles)
+    y = np.linspace(env.observation_space.low[1], env.observation_space.high[1], num=num_tiles)
+
+    # 合并x，y的信息
+    X,Y = np.meshgrid(x,y)
+    Z = np.apply_along_axis(lambda _: -np.max(estimator.predict(_)), 2,np.dstack([X,Y]))
+
+    # 配置显示图像信息
+    fig = plt.figure(figsize=(15,7.5))
+    ax = fig.add_subplot(111, projection = '3d')
+    surf = ax.plot_surface(X,Y,Z, rstride=1, cstride=1)
+
+    # 设置x轴为位置信息
+    ax.set_xlabel("position")
+    # 设置y轴为速率信息
+    ax.set_ylabel("Velocity")
+    # 设置Z轴为价值信息
+    ax.set_zlabel("Value")
+    # 设置Z轴大小，便于显示
+    ax.set_zlim(0,160)
+    # 设置背景为白色
+    ax.set_facecolor("white")
+    # 设置标题
+    ax.set_title("Cost To Go Function(iter:{}".format(niter))
+    # 设置侧边条
+    fig.colorbar(surf)
+    # 显示图像
+    plt.show()
+
+# 值函数近似法 + QLearning
+estimator = EStimator()
+# vf = VF_QLearning(env, estimator, num_episodes=100, epsilon=0.2)
+# result = vf.q_learning()
+# 值函数近似法 + QLearning + 图像
+iter = [10, 50, 100, 200]
+for x in iter:
+    vf = VF_QLearning(env, estimator, num_episodes=x, epsilon=0.2)
+    result = vf.q_learning()
+    plot_cost_to_go_mountain_car(env, estimator,x , num_tiles=10)
+
+# # Mountain-Car 的应用演示
+# observation = env.reset()
+# plt.figure()
+# for x in range(1000):
+#     action = np.random.choice([0,1,2])
+
+#     observation,reward,done,info = env.step(action)
+#     print(action,reward ,done)
+#     print(observation)
+#     time.sleep(0.02)
+    
+#     plt.imshow(env.render(mode='rgb_array'))
+# env.close()
